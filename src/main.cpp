028ef5db6d5b64f323facd934138588696bd2781
Aryadev Chavali <aryadev@aryadevchavali.com>
2023-10-12 23:39:55 +0100

    (main)+main types and a general computer type
    Provides the status registers using bitwise and.

--- src/main.cpp
+++ src/main.cpp
@@ -10,9 +10,61 @@
  * Description: The entrypoint to the program.
  */
 
+#include <cstdint>
 #include <iostream>
 
 using std::cout, std::endl;
+
+using byte = std::uint8_t;
+using u16  = std::uint16_t;
+using u64  = std::uint64_t;
+using i64  = std::int64_t;
+
+struct Machine
+{
+  u64 clock;
+  u16 PC;
+  byte A, X, Y, S;
+  byte P : 7;
+
+  byte *program;
+  byte memory[1 << 17]; // 64 KB
+
+  constexpr bool N(void)
+  {
+    return (P & 0b10000000);
+  }
+
+  constexpr bool V(void)
+  {
+    return (P & 0b01000000);
+  }
+
+  constexpr bool B(void)
+  {
+    return (P & 0b00010000);
+  }
+
+  constexpr bool D(void)
+  {
+    return (P & 0b00001000);
+  }
+
+  constexpr bool I(void)
+  {
+    return (P & 0b00000100);
+  }
+
+  constexpr bool Z(void)
+  {
+    return (P & 0b00000010);
+  }
+
+  constexpr bool C(void)
+  {
+    return (P & 0b00000001);
+  }
+};
 
 int main(void)
 {
